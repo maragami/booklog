f9f41f654c53fd6d18aba95cd53ff7d5bb28124d
maragami <maria.agamirzaeva@helsinki.fi>
2026-02-26 23:51:06 +0200

    Added show book info page

--- books.py
+++ books.py
@@ -1,9 +1,19 @@
 import db
 
 def add_book(title, author, user_id):
-    sql = "INSERT INTO books (title, author, user_id) VALUES (?, ?, ?)"
+    sql = """INSERT INTO books (title, author, user_id) VALUES (?, ?, ?)"""
     db.execute(sql, [title, author, user_id])
 
 def get_all_books():
     sql = "SELECT id, title, author FROM books ORDER BY id DESC"
     return db.query(sql)
+
+def get_book(book_id):
+    sql = """SELECT books.title,
+                    books.author,
+                    users.username
+                FROM books, users
+                WHERE books.user_id = users.id AND
+                    books.id = ?"""
+    return db.query(sql, [book_id])[0]
+    
